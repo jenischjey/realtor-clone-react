f5053652e61e4f553379f99b57ef79d51273cef3
Jenisch Jey <jenischjey@Jenischs-MacBook-Pro.local>
2023-12-17 16:18:13 -0500

    added more to sign in page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+
 import {useLocation, useNavigate} from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
+  console.log(location.pathname);
   const navigate = useNavigate();
   function pathMathRoute(route){
-    if(route === location.pathname){
+    if (route === location.pathname) {
       return true
     }
   }
@@ -13,12 +14,12 @@ export default function Header() {
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
         <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
           <div>
-            <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" 
+            <img 
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" 
             alt="logo" 
             className="h-5 cursor-pointer"
             onClick={() => navigate("/")}
             />
-
           </div>
           <div>
             <ul className="flex space-x-10">
